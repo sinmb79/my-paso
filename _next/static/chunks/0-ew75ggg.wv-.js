73e67d281bb6d1c06a3422587a96b443c642dbb1
sinmb79 <sinmb79@naver.com>
2026-04-09 01:26:28 +0900

    deploy: publish github pages

---
+++ _next/static/chunks/0-ew75ggg.wv-.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,52498,t=>{t.v(l=>Promise.all(["static/chunks/0d5l4wfvh1ru..js"].map(l=>t.l(l))).then(()=>l(71725)))}]);
--- tests/unit/next-config.test.ts
+++
@@ -1,7 +0,0 @@
-import nextConfig from "../../next.config";
-
-describe("next config", () => {
-  it("enables static export for local-first hosting", () => {
-    expect(nextConfig.output).toBe("export");
-  });
-});
