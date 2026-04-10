cf672d140955c0e569037e6dfb967a1ff998dcd0
sinmb79 <sinmb79@naver.com>
2026-04-10 20:57:53 +0900

    feat: add android capacitor platform

---
+++ capacitor.config.ts
@@ -0,0 +1,17 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.mypaso.app",
+  appName: "Hello! My Paso!",
+  webDir: "out",
+  server: {
+    androidScheme: "https",
+  },
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: false,
+    },
+  },
+};
+
+export default config;
